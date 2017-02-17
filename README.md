c3e61e5ae6efdfe570de732ba2dd7c8e0508ebb2
mycor-xyxon <xyxon.minakawa@gmail.com>
2017-02-17 17:09:29 +0900

    first commit

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # sampleSpringLeaf
-sample spring boot thymeleaf
+Thymeleaf environment for frontend
 
+## start server
 ```sh
 mvn spring-boot:run
 ```
+
+## url
+[http://localhost:8090/](http://localhost:8090/)
 
 ## doc
 - [構築手順](./doc/create.md)
--- src/main/java/edu/self/Hello.java
+++ src/main/java/edu/self/Hello.java
@@ -1,19 +1,19 @@
-package edu.self;
+// package edu.self;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
-public class Hello {
-    public static void main(String[] args) {
-        SpringApplication.run(Hello.class, args);
-    }
+// @SpringBootApplication
+// @RestController
+// public class Hello {
+//     public static void main(String[] args) {
+//         SpringApplication.run(Hello.class, args);
+//     }
 
-    @RequestMapping(value = "/")
-    String hello() {
-        return "Hello World!";
-    }
-}
+//     @RequestMapping(value = "/")
+//     String hello() {
+//         return "Hello World!";
+//     }
+// }
